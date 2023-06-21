96b57c886522122be24c74b453ef3fdf42f0a4e6
Angus <angus1angus@bigpond.com>
2023-06-22 03:48:20 +0930

    header finished

--- app/character/appearance.tsx
+++ app/character/appearance.tsx
@@ -1,11 +1,11 @@
 export default function Appearance(){
     return (
-            <h4 className="font-mono text-ellipsis">
+            <p className="font-mono text-ellipsis">
                 Appearance: <br/>
                 Physical Detail: Train cowcatcher covers lower face<br/>
                 Clothing: <br/>
                 Personality: <br/>
                 Mannerism: <br/>
-            </h4>
+            </p>
     )
 }
--- app/character/header.tsx
+++ app/character/header.tsx
@@ -2,38 +2,47 @@ export default function Header(){
     return (
             <div className="
             flex justify-between flex-row 
-            rounded-lg items-center m-2
+            rounded-lg items-center mx-2 my-3
             align-middle w-full pr-6
             ">
+                <div className="flex flex-col mr-2 
+                aspect-square border-2 rounded-lg text-2xl bg-blue-500 h-20 
+                border-white z-10 absolute left-2">
+                    <div className="h-5/6 flex flex-col justify-center text-center">
+                        <h1 className="font-mono text-4xl">3</h1>
+                    </div>
+                    <div className="h-1/6 w-full
+                    rounded-b-md bg-gray-500">
+                        <div className="bg-green-400 w-1/2 h-full rounded-bl-md"/>
+                    </div>
+                </div>
                 <div className="flex flex-row bg-slate-800 justify-start
-                p-1 border-2 border-white w-full mr-10">
+                border-2 border-white w-full mx-10 rounded-lg pl-11">
                     <h1 className="font-mono font-bold border-white mr-2
                     px-4 py-2 rounded-lg text-2xl bg-black border-2 w-60
-                    text-clip whitespace-nowrap overflow-scroll
+                    text-clip whitespace-nowrap overflow-scroll my-1
                     ">
                         Locomotive Maw
                     </h1>
-                    <div>
-                        <div className="flex justify-center">
-                            <h3 className="font-mono font-bold px-2">Level: 3</h3>
-                            <h3 className="font-mono font-bold px-2">Xp: 10</h3>
-                        </div>
-                        <div className="flex flex-row">
-                            <h4 className="font-mono px-1">Str:1</h4>
-                            <h4 className="font-mono px-1">Dex:1</h4>
-                            <h4 className="font-mono px-1">Wil:1</h4>
-                            <h4 className="font-mono px-1">Arm:6</h4>
-                        </div>
+                    <div className="flex flex-col justify-center">                        
+                        <h4 className="font-mono px-1">Str: 1 | Dex: 1</h4>                        
+                        <h4 className="font-mono px-1">Wil: 1 | Arm: 6</h4>
                     </div>
                 </div>
-                <button className="
-                rounded-full bg-red-900 p-2
-                w-20 h-20 border-2 border-white text-center
-                hover:bg-red-400 z-10 absolute right-2 overflow-hidden
-                text-5xl font-mono tabular-nums whitespace-nowrap
+                <div className="
+                rounded-full p-2 flex flex-col justify-center
+                w-20 h-20 border-2 border-white z-10 text-center
+                absolute right-2 overflow-hidden
                 ">
-                    4
-                </button>
+                    <div className=" rounded-full bg-gray-500 absolute
+                    w-20 h-20 z-20 right-0 flex flex-col justify-end">
+                        <div className=" bg-red-900
+                        w-full h-3/4 z-30">
+                        </div>
+                    </div>
+                    <h1 className="text-5xl font-mono tabular-nums 
+                    whitespace-nowrap overflow-hidden z-20">6</h1>
+                </div>
             </div>
         )
 }
